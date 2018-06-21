535f597b873fc3591d7ef517313abe2f99fd79ea
188665225 <40428973+188665225@users.noreply.github.com>
2018-06-21 15:30:12 +0800

    Create MathTest.java

---
+++ src/main/java/cn/qhit/util/MathTest.java
@@ -0,0 +1,11 @@
+package cn.qhit.util;
+
+public class MathTest {
+	public static void main(String[] args) {
+		double i =11;
+		double a = 2;
+		double d = Math.ceil(i/a);
+		System.out.println(d);
+	}
+
+}
